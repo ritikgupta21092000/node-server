f964b4c0fab2fc66f87a25833c15644e798d44f0
Ritik Gupta <guptaritik349@gmail.com>
2020-06-12 02:00:29 +0530

    Updated files

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ app.get("/", function(req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
-app.listen(3000, function() {
+app.listen(3000, () => {
   console.log("Server is running on port 3000");
-});
+})
